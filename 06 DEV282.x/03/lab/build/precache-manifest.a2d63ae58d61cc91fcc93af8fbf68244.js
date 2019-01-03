b4ee9c3939b0dffc05d4eefd5831991ecb6e71b7
Le Duy Anh <ledzanh@gmail.com>
2019-01-03 16:08:37 +0700

    :arrow_up: Update WEBM/Build Restaurant

---
+++ 06 DEV282.x/03/lab/build/precache-manifest.a2d63ae58d61cc91fcc93af8fbf68244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "043f7f71143589ff2f5f",
+    "url": "/static/js/main.043f7f71.chunk.js"
+  },
+  {
+    "revision": "69bff6a526e3f3f5cb5e",
+    "url": "/static/js/1.69bff6a5.chunk.js"
+  },
+  {
+    "revision": "043f7f71143589ff2f5f",
+    "url": "/static/css/main.7111d5c1.chunk.css"
+  },
+  {
+    "revision": "6ffb1e95abbf94d93953bcea6f91e1a9",
+    "url": "/index.html"
+  }
+];
