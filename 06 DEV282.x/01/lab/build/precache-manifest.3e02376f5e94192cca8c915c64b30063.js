a7b3bf9d11132071882763af6a03b869e657e098
Le Duy Anh <ledzanh@gmail.com>
2019-01-03 14:06:06 +0700

    :ok_hand: Build 06/Router

---
+++ 06 DEV282.x/01/lab/build/precache-manifest.3e02376f5e94192cca8c915c64b30063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5362a8add4381f9d3a5",
+    "url": "/static/js/main.c5362a8a.chunk.js"
+  },
+  {
+    "revision": "927254355be8e1f3f284",
+    "url": "/static/js/1.92725435.chunk.js"
+  },
+  {
+    "revision": "c5362a8add4381f9d3a5",
+    "url": "/static/css/main.b1a279d1.chunk.css"
+  },
+  {
+    "revision": "927254355be8e1f3f284",
+    "url": "/static/css/1.a26009e2.chunk.css"
+  },
+  {
+    "revision": "0833f57f9162c8643208bcd627a54591",
+    "url": "/index.html"
+  }
+];
